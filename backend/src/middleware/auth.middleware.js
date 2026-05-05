3a391dda96a0b6fe1f40c54681ec5fd773660535
Basele <s.basele@gmail.com>
2026-05-05 15:12:45 +0300

    Revert "Update auth.middleware.js"
    This reverts commit 40267f6253210d4b7539d376c9c03104927fd938.

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,36 +1,30 @@
 const { verifyAccessToken } = require('../utils/jwt');
 const { AppError } = require('../utils/AppError');
 
-function extractToken(req) {
+exports.authenticate = async (req, res, next) => {
   try {
-    if (!req || !req.headers) return null;
-    const auth = req.headers['authorization'];
-    if (auth && typeof auth === 'string' && auth.startsWith('Bearer ')) {
-      return auth.substring(7);
-    }
-    return null;
-  } catch {
-    return null;
-  }
-}
-
-exports.authenticate = (req, res, next) => {
-  try {
-    const token = extractToken(req);
-    if (!token) return next(new AppError('No token provided', 401));
-    req.user = verifyAccessToken(token);
+    const header = req.headers?.authorization;
+    if (!header?.startsWith('Bearer ')) throw new AppError('No token provided', 401);
+    const token = header.split(' ')[1];
+    const payload = verifyAccessToken(token);
+    req.user = payload;
     next();
   } catch (err) {
     next(err);
   }
 };
 
-exports.optionalAuth = (req, res, next) => {
+exports.optionalAuth = async (req, res, next) => {
   try {
-    const token = extractToken(req);
-    if (token) req.user = verifyAccessToken(token);
-  } catch {}
-  next();
+    const header = req.headers?.authorization;
+    if (header?.startsWith('Bearer ')) {
+      const token = header.split(' ')[1];
+      req.user = verifyAccessToken(token);
+    }
+    next();
+  } catch {
+    next();
+  }
 };
 
 exports.requireRole = (...roles) => (req, res, next) => {
